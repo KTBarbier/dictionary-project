3bda487be0a3ccba7c5c21efc42c200988f285c1
KTBarbier <93622297+KTBarbier@users.noreply.github.com>
2022-06-02 19:42:35 -0500

    set up search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by Kristen Barbier</small>
+        </footer>
+      </div>
     </div>
   );
 }
